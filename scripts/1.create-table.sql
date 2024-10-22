333fdec1dfe35e8d99c28e4fbd6ceb70e07e3e82
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-10-21 23:06:10 -0300

    feat: create table

---
+++ scripts/1.create-table.sql
@@ -0,0 +1,11 @@
+-- char (CHARACTER)
+-- varchar (VARIABLE CHARACTER)
+DROP TABLE IF EXISTS customers;
+CREATE TABLE IF NOT EXISTS customers (
+  -- Auto increment
+  id SERIAL,
+  first_name VARCHAR(20),
+  last_name VARCHAR(60),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
