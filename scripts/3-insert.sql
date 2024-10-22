e5c79457b3d61ee1d616a200d204bf6f834f5c47
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-10-22 00:00:34 -0300

    feat: select and insert

--- scripts/3-insert.sql
+++ scripts/3-insert.sql
@@ -1,3 +1,24 @@
 \c pg1
 
-INSERT INTO customers VALUES(DEFAULT, 'Vittor', 'Emanoel', DEFAULT, 'vittor@test.com')
+-- INSERT INTO customers VALUES(DEFAULT, 'Vittor', 'Emanoel', DEFAULT, 'vittor@test.com')
+
+-- INSERT INTO customers VALUES(DEFAULT, 'Vittor', 'Emanoel', DEFAULT, 'vittor@test.com')
+
+-- INSERT INTO customers VALUES (DEFAULT, 'Gustavo', 'Ramos', DEFAULT, 'guga@.com');
+
+-- INSERT INTO customers VALUES(DEFAULT, 'Beatriz', 'Galdino', DEFAULT, 'bibia@com')
+
+
+-- MANY VALUES: BATCH INSERTS
+-- INSERT INTO customers(first_name, last_name, email)
+--   VALUES
+--   ('Javacripto', 'Silva', 'javacripto@.com'),
+--   ('Rose', 'Silva', 'rose@com'),
+--   ('Reacto', 'Silva', 'reacto@.com'),
+--   ('Dora', 'Silva', 'dora@.com')
+--   ;
+
+INSERT INTO customers(first_name, last_name, email)
+  VALUES ('Javacripto', 'Silva', 'javacripto@.com')
+  RETURNING id, created_at ;
+
---
+++ scripts/4-select.sql
@@ -0,0 +1,10 @@
+\c pg1
+
+-- * = ALL
+-- SELECT * FROM customers;
+
+-- SELECT id, first_name, last_name, email FROM customers;
+
+-- SELECT id AS Id, first_name AS "firstName", last_name, email FROM customers;
+
+select * from customers;
